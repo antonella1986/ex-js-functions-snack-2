71f2ec6609891f7a50f1a414325c73339b9baa36
antonella1986 <antonella.leo.mail@gmail.com>
2025-06-04 11:31:54 +0200

    first commit

---
+++ snack1.js
@@ -0,0 +1,13 @@
+function somma(num1, num2) {
+    return num1 + num2
+}
+
+somma(1, 2)
+
+const somma = function (num1, num2) {
+    return num1 + num2
+}
+
+const sommaArrowFunction = (num1, num2) => {
+    return num1 + num2
+}
